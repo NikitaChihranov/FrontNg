37521a7bbe4b134c784e109ddb82082a5c7e60be
nikitaChihranov <nikita.chihranov@gmail.com>
2019-02-03 21:28:29 +0400

    Delete second page for users.

--- src/app/core/users/users-routing.module.ts
+++ src/app/core/users/users-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {UserLoginComponent} from './user-login/user-login.component';
 import {UserPageAdminComponent} from './user-page-admin/user-page-admin.component';
 
 const routes: Routes = [
   {path: '', component: UserPageAdminComponent},
-  {path: 'admin', component: UserLoginComponent}
 ];
 
 @NgModule({
--- src/app/core/users/users.module.ts
+++ src/app/core/users/users.module.ts
@@ -4,10 +4,9 @@ import { CommonModule } from '@angular/common';
 import { UsersRoutingModule } from './users-routing.module';
 import {UserPageAdminComponent} from './user-page-admin/user-page-admin.component';
 import {FormsModule} from '@angular/forms';
-import {UserLoginComponent} from './user-login/user-login.component';
 
 @NgModule({
-  declarations: [UserPageAdminComponent, UserLoginComponent],
+  declarations: [UserPageAdminComponent],
   imports: [
     CommonModule,
     UsersRoutingModule,
